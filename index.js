b1d73b142aeeb8f6cdb8d60c103db97962521663
Lorenzo Ercoli <l.ercoli@qzrstudio.com>
2023-04-12 17:40:18 +0200

    Inizializza il repository

---
+++ index.js
@@ -0,0 +1,10 @@
+const TelegramBot = require("node-telegram-bot-api");
+const TOKEN = process.env.TOKEN;
+
+// Crea una nuova istanza bot Telegram
+const bot = new TelegramBot(TOKEN, { polling: true });
+
+// Ascolta i messaggi
+bot.on("message", (msg) => {
+    bot.sendMessage(msg.chat.id, "Ciao, questo è il tuo primo bot telegram!");
+});
